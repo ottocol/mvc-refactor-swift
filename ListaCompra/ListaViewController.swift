cf88538b3c44f4cea11865c1fc1a25197525ea15
Otto Colomina Pardo <otto@dccia.ua.es>
2018-01-24 01:26:39 +0100

    cambiada estructura de datos a array de arrays, simplifica la manipulación

--- ListaCompra/ListaViewController.swift
+++ ListaCompra/ListaViewController.swift
@@ -9,31 +9,37 @@
 import UIKit
 
 class ListaViewController: UITableViewController {
-    var listaItems : [Item] = []
+    var listaItems : [[Item]] = [[],[],[]]
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listaItems.filter{$0.prioridad==Prioridad(rawValue:section)}.count
+        return listaItems[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celda = tableView.dequeueReusableCell(withIdentifier: "MiCelda", for: indexPath)
-        let listaFiltrada = listaItems.filter{$0.prioridad==Prioridad(rawValue:indexPath.section)}
-        celda.textLabel?.text = listaFiltrada[indexPath.row].nombre
+        let seccion = listaItems[indexPath.section]
+        let item = seccion[indexPath.row]
+        celda.textLabel?.text = item.nombre
+        if item.comprado {
+            celda.accessoryType = .checkmark
+        }
+        else {
+            celda.accessoryType = .none
+        }
         return celda
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            self.listaItems.remove(at: indexPath.row)
+            self.listaItems[indexPath.section].remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let celda = tableView.cellForRow(at: indexPath) {
-            let pos = indexPath.row
-            listaItems[pos].comprado = !listaItems[pos].comprado
-            if listaItems[pos].comprado {
+            listaItems[indexPath.section][indexPath.row].comprado = !listaItems[indexPath.section][indexPath.row].comprado
+            if listaItems[indexPath.section][indexPath.row].comprado {
                 celda.accessoryType = .checkmark
             }
             else {
@@ -56,6 +62,7 @@ class ListaViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.listaItems = leerLista()
+        print("Lista leida...\(Date())")
     }
 
     override func didReceiveMemoryWarning() {
@@ -67,23 +74,23 @@ class ListaViewController: UITableViewController {
         if segue.identifier! == "guardar" {
             let vc = segue.source as! NuevoItemViewController
             if let nuevoItem = vc.nuevoItem {
-                listaItems.append(nuevoItem)
+                listaItems[nuevoItem.prioridad.rawValue].append(nuevoItem)
                 self.tableView?.reloadData()
             }
         }
     }
     
-    func leerLista() -> [Item] {
+    func leerLista() -> [[Item]] {
         let urlDocs = FileManager.default.urls(for:.documentDirectory,
                                                in:.userDomainMask)[0]
         let urlArchivo = urlDocs.appendingPathComponent("items.plist")
         if let data = try? Data(contentsOf: urlArchivo) {
             let decoder = PropertyListDecoder()
-            let lista = try! decoder.decode([Item].self, from: data)
+            let lista = try! decoder.decode([[Item]].self, from: data)
             return lista
         }
         else {
-            let lista : [Item] = []
+            let lista : [[Item]] = [[],[],[]]
             return lista
         }
     }
